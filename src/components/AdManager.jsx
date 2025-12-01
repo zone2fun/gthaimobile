4704c47b99dbb799c932ca2708b2e42ddcc07d5c
zone2fun <zone2fun@gmail.com>
2025-12-01 20:21:04 +0700

    add ads2

--- src/components/AdManager.jsx
+++ src/components/AdManager.jsx
@@ -43,20 +43,20 @@ const AdManager = () => {
             {/* Desktop Ads - Left & Right */}
             <div className="ad-desktop ad-left">
                 <div className="ad-placeholder-vertical">
-                    <span>
-                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9202394633228299"
-                            crossorigin="anonymous"></script>
 
-                        <ins class="adsbygoogle"
-                            style="display:block"
-                            data-ad-client="ca-pub-9202394633228299"
-                            data-ad-slot="7366794636"
-                            data-ad-format="auto"
-                            data-full-width-responsive="true"></ins>
-                        <script>
-                            (adsbygoogle = window.adsbygoogle || []).push({ });
-                        </script>
-                    </span>
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9202394633228299"
+                        crossorigin="anonymous"></script>
+
+                    <ins class="adsbygoogle"
+                        style="display:block"
+                        data-ad-client="ca-pub-9202394633228299"
+                        data-ad-slot="7366794636"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({ });
+                    </script>
+
                     {/* Paste your AdSense code here */}
                     {/* <ins className="adsbygoogle" ... ></ins> */}
                 </div>
@@ -64,19 +64,19 @@ const AdManager = () => {
 
             <div className="ad-desktop ad-right">
                 <div className="ad-placeholder-vertical">
-                    <span>
-                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9202394633228299"
-                            crossorigin="anonymous"></script>
-                        <ins class="adsbygoogle"
-                            style="display:block"
-                            data-ad-client="ca-pub-9202394633228299"
-                            data-ad-slot="3772832857"
-                            data-ad-format="auto"
-                            data-full-width-responsive="true"></ins>
-                        <script>
-                            (adsbygoogle = window.adsbygoogle || []).push({ });
-                        </script>
-                    </span>
+
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9202394633228299"
+                        crossorigin="anonymous"></script>
+                    <ins class="adsbygoogle"
+                        style="display:block"
+                        data-ad-client="ca-pub-9202394633228299"
+                        data-ad-slot="3772832857"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({ });
+                    </script>
+
                     {/* Paste your AdSense code here */}
                     {/* <ins className="adsbygoogle" ... ></ins> */}
                 </div>
@@ -85,19 +85,19 @@ const AdManager = () => {
             {/* Mobile Ad - Bottom Floating */}
             <div className="ad-mobile-bottom">
                 <div className="ad-placeholder-horizontal">
-                    <span>
-                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9202394633228299"
-                            crossorigin="anonymous"></script>
-                        <ins class="adsbygoogle"
-                            style="display:block"
-                            data-ad-client="ca-pub-9202394633228299"
-                            data-ad-slot="2427961590"
-                            data-ad-format="auto"
-                            data-full-width-responsive="true"></ins>
-                        <script>
-                            (adsbygoogle = window.adsbygoogle || []).push({ });
-                        </script>
-                    </span>
+
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9202394633228299"
+                        crossorigin="anonymous"></script>
+                    <ins class="adsbygoogle"
+                        style="display:block"
+                        data-ad-client="ca-pub-9202394633228299"
+                        data-ad-slot="2427961590"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({ });
+                    </script>
+
                     {/* Paste your AdSense code here */}
                     {/* <ins className="adsbygoogle" ... ></ins> */}
                 </div>
